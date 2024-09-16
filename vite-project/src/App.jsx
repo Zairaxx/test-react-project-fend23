cbc0442f71b1f631426ef35eb49cc27e99b788e2
Zairaxx <brandontsegai@gmail.com>
2024-09-16 11:05:17 +0200

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import MyComponent from './components/MyComponent'
+
+
+function App() {
+
+  return (
+    <>
+      <h1>React projekt</h1>
+      <MyComponent/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/MyComponent.jsx
@@ -0,0 +1,13 @@
+function MyComponent(){
+
+    let myName = "Brandon";
+
+    return(
+        <>
+            <h2 className="underrubrik">{myName}'s site</h2>
+            <h3>En till rubrik</h3>
+        </>
+    )
+}
+
+export default MyComponent
