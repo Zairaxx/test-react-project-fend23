5ee018192b3df3a2109f10cdf03021dec42842ec
Zairaxx <brandontsegai@gmail.com>
2024-09-16 13:41:11 +0200

    added props and loops

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,11 +4,14 @@ import MyComponent from './components/MyComponent'
 
 function App() {
 
+
+  let myData = [1,2,3,4,5];
+
   return (
     <>
       <h1>React projekt</h1>
-      <MyComponent/>
-      
+      <MyComponent test={myData} test2="Min andra property"/>
+      <Header />
     </>
   )
 }
--- vite-project/src/components/MyComponent.jsx
+++ vite-project/src/components/MyComponent.jsx
@@ -1,11 +1,18 @@
-function MyComponent(){
+function MyComponent(props){
 
-    let myName = "Brandon";
+    let myProfile = {
+        name: "Brandon",
+        age: 30
+    }
+
 
     return(
         <>
-            <h2 className="underrubrik">{myName}'s site</h2>
+            <h2 className="underrubrik">Name: {myProfile.name} Age:{myProfile.age}</h2>
             <h3>En till rubrik</h3>
+            <ul>
+                {props.test.map(num =><li>{num}</li>)}
+            </ul>
         </>
     )
 }
